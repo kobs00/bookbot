e00e60314c00231da9fffd3fc42a7840dec22416
kobs00 <kylearbolino@gmail.com>
2024-12-12 15:54:25 +0000

    Added bookbot analysis for frankenstein text

---
+++ main.py
@@ -0,0 +1,37 @@
+def main():
+    with open(file_name) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def count_words(file_contents):
+    word_count = 0
+    for word in file_contents.split():
+        word_count += 1
+    return word_count
+
+def count_chars(file_contents):
+    char_count = {}
+    for char in file_contents.lower():
+        if char in char_count and char.isalpha():
+            char_count[char] += 1
+        elif char.isalpha():
+            char_count[char] = 1
+    return char_count
+
+
+file_name = "books/frankenstein.txt"
+char_analysis = []
+
+for char, count in sorted(count_chars(main()).items(), key=lambda item: item[1], reverse=True):
+    char_analysis.append(f"The {char} character was found {count} times")
+
+
+print(
+    f"--- Begin report of {file_name} --- \n"
+    + f"{count_words(main())} words found in the document \n"
+    + "\n".join(char_analysis)
+    + "\n" + "--- End report ---"
+)
+
+
